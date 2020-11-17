36b684bdae82388b36d7da0364662a861aadf47c
pthakar-cmu <63481718+pthakar-cmu@users.noreply.github.com>
2020-11-17 18:19:34 -0500

    Add files via upload
    Updated

--- L6 Fairness/L6_Fairness.py
+++ L6 Fairness/L6_Fairness.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import learning_curve
 from sklearn.metrics import classification_report,confusion_matrix, roc_curve, roc_auc_score, auc, accuracy_score
 from sklearn.model_selection import ShuffleSplit,train_test_split, cross_val_score, GridSearchCV
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, label_binarize, StandardScaler, MinMaxScaler
-from imblearn.over_sampling import SMOTE
 from collections import defaultdict
 from xgboost import XGBClassifier
 import matplotlib.pyplot as plt 
@@ -51,13 +50,11 @@ def get_eval2(clf, X_train, y_train,X_test, y_test):
   
 # Function to get roc curve
 def get_roc (y_test,y_pred):
-    # Compute ROC curve and ROC area for each class
     fpr = dict()
     tpr = dict()
     roc_auc = dict()
     fpr, tpr, _ = roc_curve(y_test, y_pred)
     roc_auc = auc(fpr, tpr)
-    #Plot of a ROC curve
     plt.figure()
     lw = 2
     plt.plot(fpr, tpr, color='darkorange',
@@ -86,16 +83,15 @@ def xgbclf(params, X_train, y_train,X_test, y_test):
     model.set_params(**{'n_estimators': model.best_ntree_limit})
     model.fit(X_train, y_train)
     
-    # Predict target variables y for test data
     y_pred = model.predict(X_test, ntree_limit=model.best_ntree_limit) 
   
     abclf_cm = confusion_matrix(y_test,y_pred)
     print(abclf_cm)
+    print(abclf_cm[0][0]/ (abclf_cm[0][0] + abclf_cm[1][0]))
     print (classification_report(y_test,y_pred) )
     print ('\n')
     print ("Model Final Generalization Accuracy: %.6f" %accuracy_score(y_test,y_pred) )
     
-    # Predict probabilities target variables y for test data
     y_pred_proba = model.predict_proba(X_test, ntree_limit=model.best_ntree_limit)[:,1] 
     get_roc (y_test,y_pred_proba)
     return model
@@ -108,8 +104,6 @@ def plot_featureImportance(model, keys):
   importance_frame.tail(10).plot(kind = 'barh', x = 'Feature', figsize = (8,8), color = 'orange')
 
 file = 'german.data'
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data"
-
 names = ['existingchecking', 'duration', 'credithistory', 'purpose', 'creditamount', 
          'savings', 'employmentsince', 'installmentrate', 'statussex', 'otherdebtors', 
          'residencesince', 'property', 'age', 'otherinstallmentplans', 'housing', 
@@ -119,7 +113,6 @@ data = pd.read_csv(file,names = names, delimiter=' ')
 print(data.shape)
 print (data.columns)
 data.head(10)
-
 
 # Binarize the y output for easier use of e.g. ROC curves -> 0 = 'bad' credit; 1 = 'good' credit
 data.classification.replace([1,2], [1,0], inplace=True)
@@ -150,74 +143,85 @@ for x in range(len(catvars)):
 
 #One hot encoding, create dummy variables for every category of every categorical variable
 dummyvars = pd.get_dummies(data[catvars])
-
 data_clean = pd.concat([data[numvars], dummyvars], axis = 1)
-
 print(data_clean.shape)
 
-# Unscaled, unnormalized data
 X_clean = data_clean.drop('classification', axis=1)
 y_clean = data_clean['classification']
 X_train_clean, X_test_clean, y_train_clean, y_test_clean = train_test_split(X_clean,y_clean,test_size=0.2, random_state=1)
 
-X_train_clean.keys()
-
-params={}
-xgbclf(params, X_train_clean, y_train_clean, X_test_clean, y_test_clean)
-
-params={}
-params1={
-    'n_estimators':3000,
-    'objective': 'binary:logistic',
-    'learning_rate': 0.05,
-    'gamma':0.1,
-    'subsample':0.8,
-    'colsample_bytree':0.3,
-    'min_child_weight':3,
-    'max_depth':3,
-    #'seed':1024,
-    'n_jobs' : -1
-}
-
-params2={
-    'n_estimators':3000,
-    'objective': 'binary:logistic',
-    'learning_rate': 0.005,
-    #'gamma':0.01,
-    'subsample':0.555,
-    'colsample_bytree':0.7,
-    'min_child_weight':3,
-    'max_depth':8,
-    #'seed':1024,
-    'n_jobs' : -1
-}
+params2 = {'n_estimators':3000, 'objective': 'binary:logistic', 'learning_rate': 0.005, 'subsample':0.555, 'colsample_bytree':0.7,
+           'min_child_weight':3, 'max_depth':8, 'n_jobs' : -1}
 xgbclf(params2, X_train_clean, y_train_clean, X_test_clean, y_test_clean)
 
 
-# # Oversampling
-# sm = SMOTE(ratio='auto')
-# X_train_clean_res, y_train_clean_res = sm.fit_sample(X_train_clean, y_train_clean)
 
-# # Print number of 'good' credits and 'bad credits, should be fairly balanced now
-# print("Before/After clean")
-# unique, counts = np.unique(y_train_clean, return_counts=True)
-# print(dict(zip(unique, counts)))
-# unique, counts = np.unique(y_train_clean_res, return_counts=True)
-# print(dict(zip(unique, counts)))
+#Entire DataSet
+'''
+[[ 18  41]
+ [  9 132]]
+Acc = 0.75
+
+flipping gender
+[[ 19  40]
+ [  9 132]]
+Acc = 0.755
+'''
+
+#Anti - Classification
+'''
+Age = [[ 22,  37], 
+       [ 14 ,127]];  0.745
+       
+['statussex_A91', 'statussex_A92', 'statussex_A93', 'statussex_A94']
+Sex = [[ 19,  40]
+       [  9, 132]];  0.755
+'''
+
+#Independence and Seperation
+'''
+Age >= 35  = [[ 2 23]
+              [ 0 66]];  0.747
+Age < 35   = [[16 29]
+              [ 9 56]];  0.655
+
+Sex_Female  =  [[ 19  40]
+                [  9 132]];  0.755
+Sex_Male  = [[ 22  37]
+             [ 13 128]]; 0.75
+'''
+
+#Task 3
+'''
+Age >= 40  =  [[ 4  9]
+               [ 4 43]];  0.783
+Age < 40  =  [[12 34]
+               [ 4 91]];  0.73
+TPR = 0.5/0.75
+
+Age >= 30  =  [[ 6 33]
+               [ 3 84]];  0.714
+Age < 30  = [[ 7 18]
+             [10 40]];  0.627
+TPR = 0.67/ 0.41
 
 
-# from sklearn.linear_model import LogisticRegression
+Age >= 38  =  [[ 2 14]
+               [ 0 53]]; 0.797
+Age < 38  = [[19 27]
+             [ 3 83]];  0.772
+TPR = 1/0.86
 
-# # fit, train and cross validate Decision Tree with training and test data 
-# def logregclf(X_train, y_train,X_test, y_test):
-#     print("LogisticRegression")
-#     model = LogisticRegression().fit(X_train, y_train)
-#     print(model,'\n')
+Age >= 25   =  [[ 14  26]
+                [ 10 121]]; 0.789
+Age < 25  =  [[ 3 11]
+               [ 3 13]];  0.533
+TPR = 0.58/0.5
 
-#     # Predict target variables y for test data
-#     y_pred = model.predict_proba(X_test)[:,1]
-#     get_roc (y_test,y_pred)
-#     return
+Age >= 28  =   [[14 25]
+                [ 5 98]];  0.789
+Age < 28  =   [[ 8 11]
+               [ 3 37]];  0.7627
+TPR = 0.73/0.73
+'''
 
-# # Logistic Regression
-# logregclf(X_train_clean_res, y_train_clean_res,X_test_clean, y_test_clean)
